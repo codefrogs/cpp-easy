7700e71a47d8596a3db2e16bfe2505491b7875b5
tannerj <codingfrogs@yahoo.com>
2023-01-31 21:37:46 +0000

    feat: Add hello world example

---
+++ sections/01-hello_world/README.txt
@@ -0,0 +1,9 @@
+README.txt
+Shows how to create the program.
+
+Type:
+1. make
+2. ./hello_world
+
+To clean up type:
+1. make clean
---
+++ sections/01-hello_world/hello_world.cpp
@@ -0,0 +1,20 @@
+// Codefrogs easy-cpp
+// Comments always start with '//'
+
+/* Multi line comments are within 
+   these forward slash and star markers.
+   One more line here. But you can have more! */
+ 
+#include <iostream>  // Declares a bunch of things that we might use.
+                     // In this case 'cout', and 'endl'
+
+int main()  // The start of any program.
+{
+    // Reads, using the function 'cout', print out the text
+    // 'Hello world!'
+    // where 'endl' says write out a new line
+ 
+    std::cout << "Hello world!" << std::endl; 
+   
+    return 0; // return 0 to indicate the program worked OK.
+}
