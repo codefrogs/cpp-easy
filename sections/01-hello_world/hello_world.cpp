76390172a862506219ca8183616cc8f656390709
tannerj <codingfrogs@yahoo.com>
2023-12-14 10:06:56 +0000

    fix: Clarify difference between \n and endl

--- sections/01-hello_world/hello_world.cpp
+++ sections/01-hello_world/hello_world.cpp
@@ -4,17 +4,18 @@
 /* Multi line comments are within 
    these forward slash and star markers.
    One more line here. But you can have more! */
- 
+
+// An include header
 #include <iostream>  // Declares a bunch of things that we might use.
-                     // In this case 'cout', and 'endl'
+                     // In this case 'cout'
 
 int main()  // The start of any program.
 {
     // Reads, using the function 'cout', print out the text
     // 'Hello world!'
-    // where 'endl' says write out a new line
+    // where '\n' says write out a new line
  
-    std::cout << "Hello world!" << std::endl; 
-   
+    std::cout << "Hello world!\n" ; 
+
     return 0; // return 0 to indicate the program worked OK.
 }
--- sections/03-cout/cout.cpp
+++ sections/03-cout/cout.cpp
@@ -2,7 +2,17 @@
 
 int main()
 {
-    std::cout << "This is an example of output using cout." << std::endl;
+    // 'cout' example.
+    std::cout << "This is an example of output using cout.\n" ;
+
+    // Some times people use 'endl' instead of '\n' for a new line.
+    // It's much slower, but when speed isn't an issue, then it's just
+    // a question of style.
+
+    std::cout << "This line of text uses 'endl'" << std::endl ;
+
+    // This is slower, as endl also does what is known as a 'flush'.
+    // i.e. anything pending to be shown is sent out immediately.
 
     return 0;
 }
