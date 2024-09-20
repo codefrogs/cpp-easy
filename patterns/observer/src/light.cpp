5a0b7941aa6d19999cdf97e894caeaad19420b53
tannerj <codingfrogs@yahoo.com>
2024-09-20 09:40:36 +0100

    doc: add note about coupling with observer

--- patterns/observer/src/light.cpp
+++ patterns/observer/src/light.cpp
@@ -8,8 +8,13 @@ using std::endl;
 
 void Light::update(Codefrogs::Observable *observable)
 {
+    // We have strong coupling to the oven here, so if we knew there might be other
+    // observers, we might create a interface instead.
+    // But for this simple case, we will use the concrete class.
+    //
     Oven *oven = static_cast<Oven*>(observable); // We know there's only one observable.
-
+    
+    
     if (oven->isOn()) // pull data
     {
         cout << "Light: ON" << endl;
