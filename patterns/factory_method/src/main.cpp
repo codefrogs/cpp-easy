2702313b6ba8edac8cf4f11bbbc2d8d0475792ae
tannerj <codingfrogs@yahoo.com>
2024-10-04 10:37:06 +0100

    feat: update main to with sim run calls

--- patterns/factory_method/src/main.cpp
+++ patterns/factory_method/src/main.cpp
@@ -6,6 +6,10 @@ using std::unique_ptr;
 
 int main()
 {
+    constexpr int kOneMin{60};
     unique_ptr<AircraftSim> sim_app = make_unique<AircraftSim>();
+    sim_app->loadScenario();
+    sim_app->run(kOneMin);
+
     return 0;
 }
